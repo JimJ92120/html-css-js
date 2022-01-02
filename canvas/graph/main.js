7d5fa8f52347d3e7e851060807e4e1fe7ef7052e
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-01-03 01:36:44 +0700

    feat(canvas > graph): set canvas bg and dimensions to full width

--- canvas/graph/main.js
+++ canvas/graph/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log('Hello Graph');
+window.addEventListener('load', () => {
+  const canvas = document.querySelector('#canvas');
+  if (!canvas) return;
+
+  const context = canvas.getContext('2d');
+  if (!context) return;
+
+  console.log(context);
+  const documentClientRect =  document.documentElement.getBoundingClientRect();
+
+  context.canvas.height = documentClientRect.height;
+  context.canvas.width = documentClientRect.width;
+});
