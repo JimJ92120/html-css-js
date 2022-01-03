d1b1a57f60b0f0931abea5f8283a4b361bb801c6
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-01-03 08:37:50 +0700

    feat(canvas > graph): update direction and refactor offsets to speed

--- canvas/graph/main.js
+++ canvas/graph/main.js
@@ -40,7 +40,7 @@ function drawNode(context, node) {
   context.fill();
 }
 
-function moveNode(nodes, iter, size, offsets, dimensions) {
+function moveNode(nodes, iter, size, distancePerFrame, dimensions) {
   const {
     direction,
     position: nodePosition
@@ -48,23 +48,57 @@ function moveNode(nodes, iter, size, offsets, dimensions) {
 
   // right
   if (direction[0] === 1
-    && nodePosition[0] + offsets[0] <= dimensions[0] - size
-  ) nodes[iter].position[0] = nodePosition[0] + Math.abs(offsets[0]);
+    && nodePosition[0] + distancePerFrame <= dimensions[0] - size
+  ) nodes[iter].position[0] = nodePosition[0] + Math.abs(distancePerFrame);
   // left
   if (direction[0] === 0
-    && nodePosition[0] - offsets[0] >= 0 + size
-  ) nodes[iter].position[0] = nodePosition[0] - Math.abs(offsets[0]);
+    && nodePosition[0] - distancePerFrame >= 0 + size
+  ) nodes[iter].position[0] = nodePosition[0] - Math.abs(distancePerFrame);
   // up
   if (direction[1] === 1
-    && nodePosition[1] - offsets[1] >= 0 + size
-  ) nodes[iter].position[1] = nodePosition[1] - offsets[1];
+    && nodePosition[1] - distancePerFrame >= 0 + size
+  ) nodes[iter].position[1] = nodePosition[1] - distancePerFrame;
   // down
   if (direction[1] === 0
-    && nodePosition[1] + offsets[1] <= dimensions[1] - size
-  ) nodes[iter].position[1] = nodePosition[1] + offsets[1];
+    && nodePosition[1] + distancePerFrame <= dimensions[1] - size
+  ) nodes[iter].position[1] = nodePosition[1] + distancePerFrame;
 }
-function updateNodePosition(nodes, iter, size, offsets, dimensions) {
-  moveNode(nodes, iter, size, offsets, dimensions)
+
+function updateDirection(nodes, iter, size, distancePerFrame, dimensions) {
+  const {
+    direction,
+    position: nodePosition
+  } = nodes[iter];
+
+  // right to left
+  if (direction[0] === 1
+    && nodePosition[0] + distancePerFrame >= dimensions[0] - size
+  ) {
+    nodes[iter].direction[0] = 0;
+  }
+  // left to right
+  if (direction[0] === 0
+    && nodePosition[0] - distancePerFrame <= size
+  ) {
+    nodes[iter].direction[0] = 1;
+  }
+  // up to down
+  if (direction[1] === 1
+    && nodePosition[1] - distancePerFrame <= size
+  ) {
+    nodes[iter].direction[1] = 0;
+  }
+  // down to up
+  if (direction[1] === 0
+    && nodePosition[1] + distancePerFrame >= dimensions[1] - size
+  ) {
+    nodes[iter].direction[1] = 1;
+  }
+}
+
+function updateNodePosition(nodes, iter, size, distancePerFrame, dimensions) {
+  updateDirection(nodes, iter, size, distancePerFrame, dimensions);
+  moveNode(nodes, iter, size, distancePerFrame, dimensions)
 }
 
 window.addEventListener('load', () => {
@@ -82,9 +116,10 @@ window.addEventListener('load', () => {
   const config = {
     size: 2,
     color: 'rgba(255, 255, 255, 1)',
+    distancePerFrame: Math.random() * 10,
   };
   const nodes = graph(
-    50,
+    5,
     context.canvas.height,
     context.canvas.width,
     config.size
@@ -105,10 +140,7 @@ window.addEventListener('load', () => {
         nodes,
         iter,
         config.size,
-        [
-          Math.random() * 2,
-          Math.random() * 2
-        ],
+        config.distancePerFrame,
         [
           documentClientRect.width,
           documentClientRect.height
@@ -123,8 +155,8 @@ window.addEventListener('load', () => {
   context.fillStyle = config.color;
   loop = requestAnimationFrame(animation);
 
-  setTimeout(() => {
-    cancelAnimationFrame(loop);
-    console.log('stop');
-  }, 5000);
+  // setTimeout(() => {
+  //   cancelAnimationFrame(loop);
+  //   console.log('stop');
+  // }, 5000);
 });
