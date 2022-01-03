fd937ebafb10b4e59d8e9d0cbf91e044a1faa374
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-01-03 08:22:03 +0700

    feat(canvas > graph): add directions

--- canvas/graph/main.js
+++ canvas/graph/main.js
@@ -1,8 +1,22 @@
 "use strict";
+console.log(Math.random() * 3 - 2,
+Math.random() * 3 - 2);
+
+function randomDirection() {
+  const res = Math.floor(Math.random() * 2);
+  
+  if (res < 1) return 0;
+
+  return 1;
+}
 
 function graph(count, height, width, size) {
   return [...Array(count).keys()].map(() => {
     return {
+      direction: [
+        randomDirection(),
+        randomDirection()
+      ],
       position: [
         Math.random() * width,
         Math.random() * height
@@ -28,13 +42,36 @@ function drawNode(context, node) {
   context.fill();
 }
 
-function updateNodePosition(nodes, iter, offsets) {
+function updateNodePosition(nodes, iter, size, offsets, dimensions) {
   const {
-    position: previousPosition
+    direction,
+    position: nodePosition
   } = nodes[iter];
 
-  nodes[iter].position[0] = previousPosition[0] + offsets[0];
-  nodes[iter].position[1] = previousPosition[1] + offsets[1];
+  // right
+  if (direction[0] === 1
+    && nodePosition[0] + offsets[0] <= dimensions[0] - size
+  ) {
+    nodes[iter].position[0] = nodePosition[0] + Math.abs(offsets[0]);
+  }
+  // left
+  if (direction[0] === 0
+    && nodePosition[0] - offsets[0] >= 0 + size
+  ) {
+    nodes[iter].position[0] = nodePosition[0] - Math.abs(offsets[0]);
+  }
+  // up
+  if (direction[1] === 1
+    && nodePosition[1] - offsets[1] >= 0 + size
+  ) {
+    nodes[iter].position[1] = nodePosition[1] - offsets[1];
+  }
+  // down
+  if (direction[1] === 0
+    && nodePosition[1] + offsets[1] <= dimensions[1] - size
+  ) {
+    nodes[iter].position[1] = nodePosition[1] + offsets[1];
+  }
 }
 
 window.addEventListener('load', () => {
@@ -74,9 +111,14 @@ window.addEventListener('load', () => {
       updateNodePosition(
         nodes,
         iter,
+        config.size,
         [
-          Math.random() * 10,
-          Math.random() * 10
+          Math.random() * 2,
+          Math.random() * 2
+        ],
+        [
+          documentClientRect.width,
+          documentClientRect.height
         ]
       );
       drawNode(context, node);
