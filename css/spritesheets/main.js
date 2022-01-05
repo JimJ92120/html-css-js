1e34095773b356f6f2e2f5f72cbf092b59ec87be
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-01-05 18:27:28 +0700

    feat(css > spritesheets): add input controller (keyboard) and fix css issues

--- css/spritesheets/main.js
+++ css/spritesheets/main.js
@@ -1,3 +1,42 @@
+function removeClassNames(element, classNames) {
+  classNames.map((className) => {
+    if (!element.classList.contains(className)) return;
+
+    element.classList.remove(className);
+  });
+}
+
 window.addEventListener('load', () => {
-  console.log('hello world');
+  const character = document.querySelector('#character');
+  const animationClassNames = [
+    'to-top',
+    'to-bottom',
+    'to-right',
+    'to-left'
+  ];
+  const keyNames = [
+    'ArrowUp',
+    'ArrowDown',
+    'ArrowRight',
+    'ArrowLeft'
+  ];
+
+  document.addEventListener('keydown', (e) => {
+    const currentDirection = keyNames.indexOf(e.code);
+
+    if (currentDirection < 0) return;
+
+    const classNamesToRemove = animationClassNames
+      .reduce((classNames, className, key) => {
+        if (key !== currentDirection) {
+          classNames.push(className);
+        }
+
+        return classNames;
+      }, []);
+
+    removeClassNames(character, classNamesToRemove);
+
+    return character.classList.add(animationClassNames[currentDirection]);
+  });
 });
